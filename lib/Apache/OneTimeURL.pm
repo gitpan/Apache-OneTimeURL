8f453c24dceb536c3b2687ea32675c2f8573c88f
Neil Bowers <neil@bowers.com>
2014-01-22 23:48:41 +0000

    Import of NEILB/Apache-OneTimeURL-1.32 from CPAN.
    gitpan-cpan-distribution: Apache-OneTimeURL
    gitpan-cpan-version:      1.32
    gitpan-cpan-path:         NEILB/Apache-OneTimeURL-1.32.tar.gz
    gitpan-cpan-author:       NEILB
    gitpan-cpan-maturity:     released

--- lib/Apache/OneTimeURL.pm
+++ lib/Apache/OneTimeURL.pm
@@ -9,7 +9,7 @@ use Mail::Send;
 use Apache;
 use Apache::Constants;
 
-our $VERSION = "1.31";
+our $VERSION = "1.32";
 
 sub handler {
     my ($class,$r) = @_;
@@ -158,6 +158,11 @@ and 1.2. releases of this module.
 
 Simon Cozens, C<simon@kasei.com>
 
-Copyright Simon Cozens, and released under the same terms as Perl itself.
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2003 Simon Cozens.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
